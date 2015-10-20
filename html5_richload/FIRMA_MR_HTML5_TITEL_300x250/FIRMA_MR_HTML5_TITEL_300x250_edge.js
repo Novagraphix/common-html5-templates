12aa28b58462da166a81b24899d3725aab7edb93
Stephan Jess <s.jess@novagraphix.de>
2015-10-20 14:52:14 +0200

    HTML5 Richload

---
+++ html5_richload/FIRMA_MR_HTML5_TITEL_300x250/FIRMA_MR_HTML5_TITEL_300x250_edge.js
@@ -0,0 +1,131 @@
+/*jslint */
+/*global AdobeEdge: false, window: false, document: false, console:false, alert: false */
+(function (compId) {
+
+    "use strict";
+    var im='images/',
+        aud='media/',
+        vid='media/',
+        js='js/',
+        fonts = {
+        },
+        opts = {
+            'gAudioPreloadPreference': 'auto',
+            'gVideoPreloadPreference': 'auto'
+        },
+        resources = [
+        ],
+        scripts = [
+            js+"TweenLite.min.js",
+            js+"EasePack.min.js",
+            js+"CSSPlugin.min.js"
+        ],
+        symbols = {
+            "stage": {
+                version: "6.0.0",
+                minimumCompatibleVersion: "5.0.0",
+                build: "6.0.0.400",
+                scaleToFit: "none",
+                centerStage: "none",
+                resizeInstances: false,
+                content: {
+                    dom: [
+                        {
+                            id: 'BG',
+                            type: 'rect',
+                            rect: ['0px', '0px', '300px', '250px', 'auto', 'auto'],
+                            fill: ["rgba(0,255,55,1.00)"],
+                            stroke: [0,"rgba(0,0,0,1)","none"]
+                        },
+                        {
+                            id: 'btn_kaufen',
+                            symbolName: 'btn_kaufen',
+                            type: 'rect',
+                            rect: ['186px', '70', '114', '105', 'auto', 'auto'],
+                            cursor: 'pointer'
+                        }
+                    ],
+                    style: {
+                        '${Stage}': {
+                            isStage: true,
+                            rect: ['null', 'null', '300px', '250px', 'auto', 'auto'],
+                            overflow: 'hidden',
+                            fill: ["rgba(255,255,255,1)"]
+                        }
+                    }
+                },
+                timeline: {
+                    duration: 6915,
+                    autoPlay: true,
+                    data: [
+                        [
+                            "eid764",
+                            "opacity",
+                            6500,
+                            415,
+                            "easeOutCirc",
+                            "${btn_kaufen}",
+                            '1',
+                            '0'
+                        ],
+                        [
+                            "eid763",
+                            "display",
+                            6155,
+                            0,
+                            "easeOutCirc",
+                            "${btn_kaufen}",
+                            'none',
+                            'block'
+                        ],
+                        [
+                            "eid765",
+                            "left",
+                            6155,
+                            345,
+                            "easeOutCirc",
+                            "${btn_kaufen}",
+                            '307px',
+                            '186px'
+                        ]
+                    ]
+                }
+            },
+            "btn_kaufen": {
+                version: "6.0.0",
+                minimumCompatibleVersion: "5.0.0",
+                build: "6.0.0.400",
+                scaleToFit: "none",
+                centerStage: "none",
+                resizeInstances: false,
+                content: {
+                    dom: [
+                        {
+                            rect: ['0px', '0px', '114px', '105px', 'auto', 'auto'],
+                            tag: 'img',
+                            id: 'b_kaufen',
+                            opacity: '1',
+                            type: 'image',
+                            fill: ['rgba(0,0,0,0)', 'images/b_kaufen.png', '0px', '0px']
+                        }
+                    ],
+                    style: {
+                        '${symbolSelector}': {
+                            rect: [null, null, '114px', '105px']
+                        }
+                    }
+                },
+                timeline: {
+                    duration: 0,
+                    autoPlay: true,
+                    data: [
+
+                    ]
+                }
+            }
+        };
+
+    AdobeEdge.registerCompositionDefn(compId, symbols, fonts, scripts, resources, opts);
+
+    if (!window.edge_authoring_mode) AdobeEdge.getComposition(compId).load("FIRMA_MR_HTML5_TITEL_300x250_edgeActions.js");
+})("pp2-12844647");
--- html5_richload/README.md
+++ html5_richload/README.md
@@ -7,4 +7,59 @@ Es benötigt 2 Verzeichnisse, welche wie folgt aussehen:
         $ FIRMA_XX_HTML5_**TITEL**_**WIDTH**x**HEIGHT**
         $ FIRMA_XX_HTML5_**TITEL**_**WIDTH**x**HEIGHT**_BASE
 
+### DAS BASE-VERZEICHNIS
+
 Im ``BASE``-Verzeichnis befindet sich eine ``index.html`` und eine ``manifest.js``.
+Die ``index.html`` beinhaltet CSS. Dort die Größe des WM eintragen. In der ``manifest.js`` muss nur der Name des Verzeichnisses mit der HTML5-Animation und die Größe des WM eingetragen werden.
+
+Dann beides zippen und bei FT hochladen (Basefiles).
+
+### DAS HTML5-ANIMATIONS-VERZEICHNIS
+
+In der ``index.html``, welche noch so umbenannt werden muss nach dem Erstellen des WM mit ANIMATE, muss folgendes eingegeben werden.
+
+Nach ``<!--Adobe Edge Runtime End-->``
+
+    <style>
+    #container {
+        width: 300px;
+        height: 250px;
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+
+    ft-richload {
+        width: 300px;
+        height: 250px;
+    }
+
+    #sRichload {
+        width: 300px;
+        height: 250px;
+    }
+    </style>
+    </head>
+
+    <body>
+        <div id="container">
+            <div id="Stage" class="pp2-12844647"></div>
+        </div>
+        <script type="text/javascript">
+        container = FT.query("#container");
+
+        myFT.applyClickTag(container, 1);
+        </script>
+    </body>
+
+    </html>
+
+Die ``.an``-Datei löschen und den Rest zippen und bei FT hochladen (Richload-Files)
+
+## USAGE
+
+Beispieldateien und Verzeichnisse liegen hier im Ordner, sowie eine Vorlage für ein MR.
+
+## CHANGELOG
+
+* 20.10.2015:   Initial Upload
